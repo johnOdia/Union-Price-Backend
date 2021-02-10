22eb863be1498d9b5dffe7a546c3d1fa17cc5026
johnOdia <63448302+johnOdia@users.noreply.github.com>
2021-02-10 04:16:36 -1100

    Update app.js
    integrated model api

--- app.js
+++ app.js
@@ -1,73 +1,79 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5500;
-const Joi = require('@hapi/joi')
-const predictPrice = require('./model/model')
-const swaggerJsdoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
+const Joi = require("@hapi/joi");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const request = require("request");
+const cors = require('cors')
 
 //Swagger Schema
 const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: "Union Price API", 
-            version: "3.0.3",
-            description: "Union Price API information",
-            swagger: "2.0",
-            contact: {
-                name: "John Odia, Eugenia Ikwuegbu"
-            },
-            servers: ["http://localhost:5500", "http://unionpriceapi.herokuapp.com/"]
-        }
+  swaggerDefinition: {
+    info: {
+      title: "Union Price API",
+      version: "1.0.0",
+      description: "Union Price API information",
+      swagger: "2.0",
+      contact: {
+        name: "John Odia, Eugenia Ikwuegbu",
+      },
+      servers: ["http://localhost:5500", "http://unionpriceapi.herokuapp.com/"],
     },
-    apis: ["app.js"]
+  },
+  apis: ["app.js"],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 //Middlewares
-app.use(express.json())
-app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-app.use('/',validateReqQuery)
-
+app.use(cors())
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// app.use("/", validateReqQuery);
 
 //Validate request query with Joi
 const schema = Joi.object({
-    location: Joi.string().required(),
-    houseType: Joi.string().required(),
-    bedrooms:  Joi.number().integer().min(1).max(5).required(),
-    bathrooms: Joi.number().integer().min(1).max(5).required(),
-    toilets:  Joi.number().integer().min(1).max(5).required()
+  location: Joi.string().required(),
+  houseType: Joi.string().required(),
+  bedrooms: Joi.number().integer().min(1).max(9).required(),
+  bathrooms: Joi.number().integer().min(1).max(9).required(),
+  toilets: Joi.number().integer().min(1).max(9).required(),
 });
 
-function validateReqQuery(req,res,next){
-    const { error } = schema.validate(req.query)
-    if(error) return res.status(400).send(error.details[0].message)
-    else next()
+function validateReqQuery(req, res, next) {
+  const { error } = schema.validate(req.query);
+  if (error) return res.status(400).send(error.details[0].message);
+  else next();
 }
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 //Routes =>
 
 /**
  * @swagger
  *  /estimated-rent:
- *  get: 
+ *  post:
  *   description: Used to request estimated rent price for a given set of input parameters
  *   parameters:
  *   - name: location
- *     in: query
+ *     in: body
  *     required: true
  *   - name: houseType
- *     in: query
+ *     in: body
  *     required: true
  *   - name: bedrooms
- *     in: query
+ *     in: body
  *     required: true
  *   - name: bathrooms
- *     in: query
+ *     in: body
  *     required: true
  *   - name: toilets
- *     in: query
+ *     in: body
  *     required: true
  *   responses:
  *    200:
@@ -75,39 +81,52 @@ function validateReqQuery(req,res,next){
  *    400:
  *     description: An invalid request
  */
-app.get("/estimated-rent", (req, res) => {
-    console.log(req.query);
-    
-    const { location, houseType, bedrooms, bathrooms, toilets } = req.body
-    try {
-        const randomPrice = predictPrice(location,houseType,bedrooms,bathrooms,toilets)
-        res.status(200).json({"estimated-rent":randomPrice});
-    } catch (error) {
-        res.status(400).send('invalid request')
-    }
-})
+app.post("/estimated-rent", (req, res, next) => {
+  const { location, houseType, bedroom, bathroom, toilet} = req.body
 
+  const options = {
+    method: "POST",
+    url: "https://rent-pred.herokuapp.com/predict",
+    headers: {},
+    formData: {
+      "house-type": houseType,
+      locations: location,
+      bedroom: bedroom,
+      bathroom: bathroom,
+      toilet: toilet,
+    },
+  };
+
+  request(options, function (error, response) {
+    if (error) {
+      const err = new Error(error);
+      res.status(401).send(err.message)
+    }
+    res.send(response.body);
+  });
+
+});
 
 /**
  * @swagger
  *  /estimated-price:
- *  get: 
- *   description: Used to request estimated rent price for a given set of input parameters
+ *  post:
+ *   description: Used to request estimated price for purchasing a house according to a given set of input parameters
  *   parameters:
  *   - name: location
- *     in: query
+ *     in: body
  *     required: true
  *   - name: houseType
- *     in: query
+ *     in: body
  *     required: true
  *   - name: bedrooms
- *     in: query
+ *     in: body
  *     required: true
  *   - name: bathrooms
- *     in: query
+ *     in: body
  *     required: true
  *   - name: toilets
- *     in: query
+ *     in: body
  *     required: true
  *   responses:
  *    200:
@@ -115,14 +134,30 @@ app.get("/estimated-rent", (req, res) => {
  *    400:
  *     description: An invalid request
  */
-app.get('/estimated-price', (req, res) => {
-    const { location, houseType, bedrooms, bathrooms, toilets } = req.body
-    try {
-        const randomCost = predictPrice(location,houseType,bedrooms,bathrooms,toilets)
-        res.status(200).json([{"estimated-price": randomCost}])
-    } catch (error) {
-        res.status(400).send('invalid request')
+app.post("/estimated-price", (req, res) => {
+  const { location, houseType, bedroom, bathroom, toilet } = req.body
+
+  const options = {
+    method: "POST",
+    url: "https://sale-prediction.herokuapp.com/predict",
+    headers: {},
+    formData: {
+      "house-type": houseType,
+      locations: location,
+      bedroom: bedroom,
+      bathroom: bathroom,
+      toilet: toilet,
+    },
+  };
+
+  request(options, function (error, response) {
+    if (error) {
+      const err = new Error(error);
+      res.status(401).send(err.message)
     }
-})
+    res.send(response.body);
+  });
+});
+
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
